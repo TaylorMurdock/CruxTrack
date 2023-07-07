bb6f1dcd15121705d503c8f9609daf670875d5d0
Taylor Murdock <tjmurd4545@gmail.com>
2023-07-06 22:37:44 -0400

    made some changes to the myroute page. took out id

--- controllers/routeController.js
+++ controllers/routeController.js
@@ -5,7 +5,7 @@ const Route = require("../models/route");
 // GET /cruxtrack/myRoutes - List of Routes
 router.get("/myRoutes", async (req, res) => {
   try {
-    const routes = await Route.find(); // Fetch all routes from the database
+    const routes = await Route.find();
     res.render("myRoutes", { routes });
   } catch (error) {
     console.error("Error fetching routes:", error);
@@ -17,13 +17,13 @@ router.get("/myRoutes", async (req, res) => {
 router.get("/route/:id", async (req, res) => {
   try {
     const routeId = req.params.id;
-    const route = await Route.findById(routeId); // Fetch the specific route by its ID
+    const route = await Route.findById(routeId);
 
     if (!route) {
       return res.status(404).send("Route not found");
     }
 
-    res.render("routeDetails", { route });
+    res.render("routeDetails", { routeId, route });
   } catch (error) {
     console.error("Error fetching route details:", error);
     res.status(500).send("Internal Server Error");
